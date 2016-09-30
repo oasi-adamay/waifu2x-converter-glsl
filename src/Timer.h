f37d74d20f272aae6227aa52e1fc8bbce2272375
oasi.adamay <oasi.adamay@gmail.com>
2016-09-30 11:34:09 +0900

    update VC2015 / opencv 3.0

--- src/Timer.h
+++ src/Timer.h
@@ -2,6 +2,7 @@
 #define _TIMER_H_
 
 //-----------------------------------------------------------------------------
+#if 0
 class Timer{
 private:
 	cv::TickMeter meter;
@@ -10,5 +11,15 @@ public:
 	Timer(std::string _msg){ msg = _msg;  meter.start(); }
 	~Timer(void){ meter.stop(); std::cout << msg << meter.getTimeMilli() << "[ms]" << std::endl; }
 };
+#else
+class Timer {
+private:
+//	cv::TickMeter meter;
+	std::string msg;
+public:
+	Timer(std::string _msg) { msg = _msg; }
+	~Timer(void) { ; }
+};
+#endif
 
 #endif
